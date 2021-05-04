f7e93c63a1c31dd77379c5073f2b8f8f1bc993a2
alexdev953 <alexdev953@gmail.com>
2021-05-04 14:54:48 +0300

    added view translate

--- bot.py
+++ bot.py
@@ -20,18 +20,16 @@ async def take_start(message):
 async def echo(message: types.Message):
     answer = FindMovies().find_movies(url=message.text)
     print(answer)
-    # print(len(answer))
-    inline_declar = types.InlineKeyboardMarkup(row_width=5)
-    inline_key = []
-    for title in answer['quality'].keys():
-        for href in answer['quality'][title]['mp4']:
-            inline_key.append((title, href))
-            # inline_declar.add(types.InlineKeyboardButton)
-
-    declar_keys = (types.InlineKeyboardButton(title, url=href) for title, href in tuple(inline_key))
-    inline_declar.add(*declar_keys)
     poster_url = answer['poster']
-    await message.answer_photo(photo=poster_url, reply_markup=inline_declar)
+    await message.answer_photo(photo=poster_url)
+    for watch in answer['watch']:
+        inline_declar = types.InlineKeyboardMarkup(row_width=5)
+        inline_key = []
+        for title in watch['quality'].keys():
+            inline_key.append((title, watch['quality'][title]['mp4']))
+        declar_keys = (types.InlineKeyboardButton(title, url=href) for title, href in tuple(inline_key))
+        inline_declar.add(*declar_keys)
+        await message.answer(watch['name'], reply_markup=inline_declar)
 
 
 if __name__ == '__main__':
--- find_movie_bot.py
+++ find_movie_bot.py
@@ -12,38 +12,21 @@ class FindMovies:
 
     def __init__(self):
         self.url = ''
+        self.player = None
+        self.players_url = None
+        self.script = None
+
         self.DICT_WITH_MOVIES = {
             "poster": "",
             "about": "",
-            "quality": {
-                "240p": {
-                    "m3u8": [],
-                    "mp4": []
-                },
-                "360p": {
-                    "m3u8": [],
-                    "mp4": []
-                },
-                "480p": {
-                    "m3u8": [],
-                    "mp4": []
-                },
-                "720p": {
-                    "m3u8": [],
-                    "mp4": []
-                },
-                "1080p": {
-                    "m3u8": [],
-                    "mp4": []
-                }
-            }
+            "watch": []
         }
 
     def find_movies(self, url):
         self.url = url
         self.players_url = self.find_players_url()
-        self.script = self.get_players_script()
-        return self.parse_script()
+        self.DICT_WITH_MOVIES['watch'] = self.take_translate()
+        return self.DICT_WITH_MOVIES
 
     def find_players_url(self) -> list:
         players_url = []
@@ -61,37 +44,71 @@ class FindMovies:
         poster_url = soup.find("div", {"class": "mobile_cover"}).find('img')
         self.DICT_WITH_MOVIES['poster'] = poster_url['src']
 
-    def get_players_script(self) -> str:
-        response = requests.get(self.find_players_url()[0], headers=HEADERS)
+    def get_players_script(self, url):
+        response = requests.get(url, headers=HEADERS)
         soup = BeautifulSoup(response.text, "html.parser")
         script = soup.find('body').find('script').string
-        return script
+        return self.parse_script(script=script)
 
-    def parse_script(self) -> dict:
+    def take_translate(self) -> list:
+        translate_list = []
+        response = requests.get(self.find_players_url()[0], headers=HEADERS)
+        soup = BeautifulSoup(response.text, "html.parser")
+        find_translate = soup.find("select", {"id": "translator-name"}).find_all('option')
+        for val in find_translate:
+            if val['data-token']:
+                quality = self.get_players_script(f'https://voidboost.net/movie/{val["data-token"]}/iframe?h=baskino.me')
+                translate_list.append({'name': val.text, 'quality': quality['quality']})
+        print(translate_list)
+        return translate_list
+
+    def parse_script(self, script) -> dict:
+        quality_dict = {"quality": {
+            "240p": {
+                "m3u8": [],
+                "mp4": []
+            },
+            "360p": {
+                "m3u8": [],
+                "mp4": []
+            },
+            "480p": {
+                "m3u8": [],
+                "mp4": []
+            },
+            "720p": {
+                "m3u8": [],
+                "mp4": []
+            },
+            "1080p": {
+                "m3u8": [],
+                "mp4": []
+            }
+        }}
         regex = re.compile('\n^(.*?):(.*?)$|,', re.MULTILINE)
-        js_text = re.findall(regex, self.script)  # find first item
+        js_text = re.findall(regex, script)  # find first item
         for val in js_text:
             if val[0].find('file') != -1:
                 for quality in val[1].replace('"', '').split(','):
                     good_string = quality.replace(' ', '')
                     if good_string.startswith('[240p]'):
                         extens = good_string.replace('[240p]', '').split('or')
-                        self.DICT_WITH_MOVIES['quality']['240p']['m3u8'].insert(0, extens[0])
-                        self.DICT_WITH_MOVIES['quality']['240p']['mp4'].insert(0, extens[1])
+                        quality_dict['quality']['240p']['m3u8'] = extens[0]
+                        quality_dict['quality']['240p']['mp4'] = extens[1]
                     elif good_string.startswith('[360p]'):
                         extens = good_string.replace('[360p]', '').split('or')
-                        self.DICT_WITH_MOVIES['quality']['360p']['m3u8'].insert(0, extens[0])
-                        self.DICT_WITH_MOVIES['quality']['360p']['mp4'].insert(0, extens[1])
+                        quality_dict['quality']['360p']['m3u8'] = extens[0]
+                        quality_dict['quality']['360p']['mp4'] = extens[1]
                     elif good_string.startswith('[480p]'):
                         extens = good_string.replace('[480p]', '').split('or')
-                        self.DICT_WITH_MOVIES['quality']['480p']['m3u8'].insert(0, extens[0])
-                        self.DICT_WITH_MOVIES['quality']['480p']['mp4'].insert(0, extens[1])
+                        quality_dict['quality']['480p']['m3u8'] = extens[0]
+                        quality_dict['quality']['480p']['mp4'] = extens[1]
                     elif good_string.startswith('[720p]'):
                         extens = good_string.replace('[720p]', '').split('or')
-                        self.DICT_WITH_MOVIES['quality']['720p']['m3u8'].insert(0, extens[0])
-                        self.DICT_WITH_MOVIES['quality']['720p']['mp4'].insert(0, extens[1])
+                        quality_dict['quality']['720p']['m3u8'] = extens[0]
+                        quality_dict['quality']['720p']['mp4'] = extens[1]
                     elif good_string.startswith('[1080p]'):
                         extens = good_string.replace('[1080p]', '').split('or')
-                        self.DICT_WITH_MOVIES['quality']['1080p']['m3u8'].insert(0, extens[0])
-                        self.DICT_WITH_MOVIES['quality']['1080p']['mp4'].insert(0, extens[1])
-        return self.DICT_WITH_MOVIES
+                        quality_dict['quality']['1080p']['m3u8'] = extens[0]
+                        quality_dict['quality']['1080p']['mp4'] = extens[1]
+        return quality_dict

e34e52274d4238e868d4f3eb7556ec2917323b7e
alexdev953 <alexdev953@gmail.com>
2021-05-08 09:43:27 +0300

    update in code

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from config import bot_token
 from find_movie_bot import FindMovies
 from bot_utils import make_inline_keyboard
 from db_func import DbFunc
+
 # Initialize bot and dispatcher
 bot = Bot(token=bot_token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -12,9 +13,7 @@ url_for_search = 'http://baskino.me/films/'
 
 @dp.message_handler(commands=['start'])
 async def take_start(message: types.Message):
-    user_cred = message.from_user
-    DbFunc().check_user(user_id=user_cred.id, username=user_cred.username,
-                        firstname=user_cred.first_name, lastname=user_cred.last_name)
+    DbFunc().check_user(message)
     about_bot = await bot.get_me()
     await message.answer(
         f"Привіт, {message.from_user.first_name}!\nЯ - <b>{about_bot['first_name']}</b>")
@@ -22,7 +21,7 @@ async def take_start(message: types.Message):
 
 @dp.message_handler(text_startswith=[url_for_search])
 async def echo(message: types.Message):
-    DbFunc().last_uses(message.from_user.id)
+    DbFunc().check_user(message)
     answer = FindMovies().find_movies(url=message.text)
     poster_url = answer['poster']
     await message.answer_photo(photo=poster_url)
@@ -32,7 +31,7 @@ async def echo(message: types.Message):
 
 @dp.message_handler(content_types=['text'])
 async def take_text(message: types.Message):
-    DbFunc().last_uses(message.from_user.id)
+    DbFunc().check_user(message)
     if message.text.startswith('/'):
         await message.reply('Перевірьте чи правильно введена команда')
     else:
--- db_func.py
+++ db_func.py
@@ -12,18 +12,17 @@ class DbFunc:
         cur = self.conn.cursor()
         return cur
 
-    def check_user(self, user_id, username, firstname, lastname):
+    def check_user(self, message):
+        user_cred = message.from_user
         # logger.debug(f'Check USER {user_id}, {first_name}, {user_name}')
-        self.cur.execute(f"select userid from users where userid = {user_id}")
+        self.cur.execute(f"select userid from users where userid = {user_cred.id}")
         info = self.cur.fetchall()
         if not info:
-            self.cur.execute(f"insert into users (userid, username, firstname, lastname) values ('{user_id}', '{username}', '{firstname}', '{lastname}')")
+            self.cur.execute(f"insert into users (userid, username, firstname, lastname) values "
+                             f"('{user_cred.id}', '{user_cred.username}', '{user_cred.first_name}', '{user_cred.last_name}')")
             self.conn.commit()
             self.conn.close()
         else:
-            self.last_uses(user_id)
-
-    def last_uses(self, user_id):
-        self.cur.execute(f"update users set last_uses = datetime('now', 'localtime') where userid = {user_id};")
-        self.conn.commit()
-        self.conn.close()
+            self.cur.execute(f"update users set last_uses = datetime('now', 'localtime') where userid = {user_cred.id};")
+            self.conn.commit()
+            self.conn.close()

0fab2b9db07445aea0049e204d3c45370651b561
alexdev953 <alexdev953@gmail.com>
2021-05-09 14:01:49 +0300

    bug fixes in quality

--- bot_utils.py
+++ bot_utils.py
@@ -7,7 +7,9 @@ def make_inline_keyboard(data):
         inline_declar = types.InlineKeyboardMarkup(row_width=5)
         inline_key = []
         for title in watch['quality'].keys():
-            inline_key.append((title, watch['quality'][title]['mp4']))
+            mp4_file = watch['quality'][title]['mp4']
+            if mp4_file:
+                inline_key.append((title, mp4_file))
         declar_keys = (types.InlineKeyboardButton(title, url=href) for title, href in tuple(inline_key))
         object_col.append((watch['name'], inline_declar.add(*declar_keys)))
     return object_col

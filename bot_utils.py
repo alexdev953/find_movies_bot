4d07b79fbbf7fb60f8ee769f5878cbc661a9430b
alexdev953 <alexdev953@gmail.com>
2021-08-07 07:53:41 +0300

    added catch exception

--- bot_utils.py
+++ bot_utils.py
@@ -14,18 +14,20 @@ class NextStep(StatesGroup):
     waiting_for_movies_name = State()
 
 
-
 def make_inline_keyboard(data):
     object_col = []
     for watch in data['watch']:
-        inline_declar = types.InlineKeyboardMarkup(row_width=5)
-        inline_key = []
-        for title in watch['quality'].keys():
-            mp4_file = watch['quality'][title]['mp4']
-            if mp4_file:
-                inline_key.append((title, mp4_file))
-        declar_keys = (types.InlineKeyboardButton(title, url=href) for title, href in tuple(inline_key))
-        object_col.append((watch['name'], inline_declar.add(*declar_keys)))
+        try:
+            inline_declar = types.InlineKeyboardMarkup(row_width=5)
+            inline_key = []
+            for title in watch['quality'].keys():
+                mp4_file = watch['quality'][title]['mp4']
+                if mp4_file:
+                    inline_key.append((title, mp4_file))
+            declar_keys = (types.InlineKeyboardButton(title, url=href) for title, href in tuple(inline_key))
+            object_col.append((watch['name'], inline_declar.add(*declar_keys)))
+        except Exception as e:
+            continue
     return object_col
 
 
